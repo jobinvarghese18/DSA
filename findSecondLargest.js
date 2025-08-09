eeef85f46fb951659ab80e03ebea8ec7f0ba7632
Jobin Tv <jobinvarghese18@gmail.com>
2025-08-09 11:54:28 +0530

    f/find second largest

---
+++ findSecondLargest.js
@@ -0,0 +1,18 @@
+const findSecondLargest = (arr) => {
+  let largest = 0;
+  let secondLargest = 0;
+
+  for (const element of arr) {
+    if (element > largest) {
+      largest = element;
+    }
+
+    if (element < largest && element > secondLargest) {
+      secondLargest = element;
+    }
+  }
+  return secondLargest;
+};
+
+const input = [10, 2, 100, 10, 40, 22, 99, 3];
+console.log(findSecondLargest(input));
