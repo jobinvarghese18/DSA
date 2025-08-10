2cf024cfbdd7d44bbb91daa499fd9c54a92a0c48
Jobin Tv <jobinvarghese18@gmail.com>
2025-08-10 21:37:06 +0530

    f/Star pattern

---
+++ DSA/playground/array/starPattern-2.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 5; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += j;
+  }
+  console.log(row);
+}
+
+// output:
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
