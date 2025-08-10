de609ad81c5e986b409fa6d346c09a33a371622a
Jobin Tv <jobinvarghese18@gmail.com>
2025-08-10 12:49:27 +0530

    f/Star pattern two questions resolved

---
+++ starPattern-4N.js
@@ -0,0 +1,18 @@
+const generateStarPattern = () => {
+  for (let i = 0; i < 4; i++) {
+    let row = "";
+    for (j = 0; j < 4; j++) {
+      row += " * ";
+    }
+    console.log(row);
+  }
+};
+
+generateStarPattern();
+
+// Output:
+
+// *  *  *  *
+// *  *  *  *
+// *  *  *  *
+// *  *  *  *
